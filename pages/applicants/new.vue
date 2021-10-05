2dae23c95f2d72e917ee0cbc8ed4737080fa6245
IngyinMay <ingyinmay@cgm-myanmar.com>
2021-10-05 16:16:19 +0630

    first_commit_2021_10_05

---
+++ pages/applicants/new.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <b-form v-if="show">
+            <b-form-group 
+                id="input-group-1"
+                label="Email address :"
+                label-for="input-1"
+                description="We'll never share your email with anyone else."
+            >
+                <b-form-input
+                        id="input-email"
+                        v-model="form.email"
+                        type="email"
+                        placeholder="Enter email"
+                        required
+                    ></b-form-input>
+            </b-form-group>
+        </b-form>
+    </div>
+</template>
+
+<script src="~/services/pages/applicants/new.js"></script>
